8c1e65e8a18d9dbb3f924f2bc199d933103e84db
Diego Guraieb <dgub@pm.me>
2020-05-29 20:38:16 +0100

    Fix obj->alist

--- closal.lisp
+++ closal.lisp
@@ -28,14 +28,14 @@
     (cons  obj)
     (string obj)
     (number obj)
-    (list  (map 'list #'obj->alist obj))
-    (vector (map 'vector #'obj->alist obj))
+    (list  (map 'list #'obj-alist obj))
+    (vector (map 'vector #'obj-alist obj))
     (t (let ((c (find-class (type-of obj) nil)))
       (if c
         (map 'list 
              #'(lambda (slot)
                 (let ((name (closer-mop:slot-definition-name slot)))
-                `( ,(intern (string-upcase name) :keyword) . ,(obj->alist(slot-value obj name)))))
+                `( ,(intern (string-upcase name) :keyword) . ,(obj-alist(slot-value obj name)))))
           (closer-mop:class-slots c))           
           obj)))
     ))
